459569ca35b2e09305570d0134f9f90571a31592
morilli <rcygseren@gmail.com>
2024-10-29 22:19:00 +0800

    feature[go]:init configuration

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,44 @@
+return {
+  { -- nvim-treesitter
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "go",
+      },
+    },
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = { -- lsp server protocol
+      servers = { gopls = {} },
+    },
+  },
+}
