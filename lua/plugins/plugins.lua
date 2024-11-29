deb88eac9ed29a9fe8f44b73615124f149405a3b
i_lichenhao <i_lichenhao01@shizhuang-inc.com>
2024-11-29 16:25:08 +0800

    feature:dewu modify

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,4 +1,32 @@
 return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "shellcheck",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "css-lsp",
+      })
+    end,
+  },
+  { -- trouble.nvim symbol outline
+    "folke/trouble.nvim",
+    cmd = { "Trouble" },
+    opts = {
+      modes = {
+        symbols = { -- Configure symbols mode
+          win = {
+            type = "split", -- split window
+            relative = "win", -- relative to current window
+            position = "right", -- right side
+            size = 0.3, -- 30% of the window
+          },
+        },
+      },
+    },
+  },
   { -- nvim-treesitter
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -29,32 +57,81 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      require("go").setup()
+      require("go").setup({
+        tag_transform = "snakecase",
+      })
     end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    keys = {
+      { "<leader>Gfs", "<cmd>GoFillStruct<cr>", desc = "Go Fill Struct" },
+      { "<leader>Gat", "<cmd>GoAddTag<cr>", desc = "Go Add Tag" },
+    },
   },
   {
     "neovim/nvim-lspconfig",
-    opts = { -- lsp server protocol
-      servers = { gopls = {} },
-    },
-  },
-  { -- toggle terminal
-    "akinsho/toggleterm.nvim",
-    config = true,
-    cmd = "ToggleTerm",
-    keys = { { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
     opts = {
-      open_mapping = [[<C-\>]],
-      direction = "float",
-      shade_filetypes = {},
-      hide_numbers = true,
-      insert_mappings = true,
-      terminal_mappings = true,
-      start_in_insert = true,
-      close_on_exit = true,
+      inlay_hints = { enabled = true },
+      ---@type lspconfig.options
+      servers = {
+        cssls = {},
+        tailwindcss = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+        },
+        tsserver = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+          single_file_support = false,
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "literal",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+        html = {},
+        gopls = {},
+        clangd = {},
+      },
+    },
+    { -- toggle terminal
+      "akinsho/toggleterm.nvim",
+      config = true,
+      cmd = "ToggleTerm",
+      keys = { { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
+      opts = {
+        open_mapping = [[<C-\>]],
+        direction = "float",
+        shade_filetypes = {},
+        hide_numbers = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        start_in_insert = true,
+        close_on_exit = true,
+      },
+    },
+    {
+      "olimorris/onedarkpro.nvim",
+      priority = 1000, -- Ensure it loads first
     },
   },
 }
