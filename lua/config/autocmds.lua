22ecd4f54b431e8af08e30a61d54dcb5508f26fb
lichenhaobj <lichenhaobj@kanyun.com>
2025-06-11 13:19:31 +0800

    fix: update mason-lspconfig.nvim
    debug
    
    fix

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-
 -- Run gofmt + goimports on save
 
+-- Run gofmt on save
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
@@ -11,4 +11,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     require("go.format").gofmt()
   end,
   group = format_sync_grp,
+})
+
+-- Autocmd: Set filetype to nginx for files in nginx-conf directory
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*/nginx-conf/*", "*/conf.d/*" },
+  callback = function()
+    vim.bo.filetype = "nginx"
+  end,
 })
---
+++ lua/plugins/avante/init.lua
@@ -0,0 +1,64 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false, -- 永远不要将此值设置为 "*"！永远不要！
+  opts = {
+    -- 在此处添加任何选项
+    -- 例如
+    provider = "openrouter",
+    providers = {
+      openrouter = {
+        __inherited_from = "openai",
+        endpoint = "https://openrouter.ai/api/v1",
+        api_key_name = "OPENROUTER_API_KEY",
+        model = "gpt-4o-mini",
+      },
+      openai = {
+        endpoint = "https://openrouter.ai/api/v1",
+        api_key_name = "OPENROUTER_API_KEY",
+        model = "gpt-4o",
+      },
+    },
+  },
+  -- 如果您想从源代码构建，请执行 `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- 对于 Windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- 以下依赖项是可选的，
+    "echasnovski/mini.pick", -- 用于文件选择器提供者 mini.pick
+    "nvim-telescope/telescope.nvim", -- 用于文件选择器提供者 telescope
+    "hrsh7th/nvim-cmp", -- avante 命令和提及的自动完成
+    "ibhagwan/fzf-lua", -- 用于文件选择器提供者 fzf
+    "nvim-tree/nvim-web-devicons", -- 或 echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- 用于 providers='copilot'
+    {
+      -- 支持图像粘贴
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- 推荐设置
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- Windows 用户必需
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- 如果您有 lazy=true，请确保正确设置
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
